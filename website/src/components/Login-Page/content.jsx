dea05d1d390c5f2f22b20f9b384bee274c6fe109
Aldi Saputra Rahmawan <11191007@student.itk.ac.id>
2023-03-11 10:19:29 +0800

    finished login page

--- website/src/components/Login-Page/content.jsx
+++ website/src/components/Login-Page/content.jsx
@@ -1,31 +1,45 @@
 import React from "react";
-
 import Background from "../../assets/img/Background-Login.png";
-
-import {Container, Row, Col, Image} from "react-bootstrap";
-
-import "./style.css"
+import {Container, Row, Col, Image, Button} from "react-bootstrap";
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput
+} from 'mdb-react-ui-kit';
+import "./login-style.css";
+import Logo from "../../assets/img/Logo-Astra-Abu.png";
 
 function Content() {
-
-    const styles = {
-        backgroundImage: `url(${Background})`,
-        backgroundSize: 'cover !important',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        height: '100vh',
-    };
-
     return (
-        <Container fluid style={styles}>
-            <Row>
-                <Col md={12} className="title">
-                    <p>Sistem Informasi Laporan AHASS Kaltim 1</p>
-                </Col>
-            </Row>
-        </Container>
+        <div className="background-image">
+            <Container fluid>
+                <Row>
+                    <Col md={12} className="title">
+                        <Container className="justify-content-center mx-auto">Sistem Informasi Laporan AHASS Kaltim 1</Container>
+                    </Col>
+                    <MDBContainer fluid>
+                        <MDBRow className='d-flex justify-content-center align-items-center h-50'>
+                            <MDBCol col='12'>
+                                <MDBCard className='my-5 mx-auto' style={{borderRadius: '1rem', width:"50%", backgroundColor:"#D2D2D2"}}>
+                                    <MDBCardBody className='p-5 w-100 d-flex flex-column'>
+                                        <Image src={Logo} className='mx-auto mb-3' style={{width:"50%", alignItems:"center"}}/>
+                                        <MDBInput wrapperClass='mb-1 w-50 mx-auto' label={<p style={{fontFamily:"serif", fontSize:"14px"}}>Username</p>} id='form1' type='' size="lg"/>
+                                        <MDBInput wrapperClass='mb-1 w-50 mx-auto' label={<p style={{fontFamily:"serif", fontSize:"14px"}}>Password</p>} id='form1' type='password' size="lg"/>
+                                        <Button className="sm mx-auto w-50 mb-5" style={{backgroundColor:"#820000"}}>
+                                            Login
+                                        </Button>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+                </Row>
+            </Container>
+        </div>
     );
-                    
 }
 
 export default Content;
